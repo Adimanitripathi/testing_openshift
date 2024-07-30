2570a2ba44fffb60cca175716e53d6a394a4c0cf
Aditya Mani Tripathi <137162244+Adimanitripathi@users.noreply.github.com>
2024-07-30 14:36:46 +0530

    third commit

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from fastapi import HTTPException
+from flask import Flask, request, jsonify
 from typing import  Dict
 import os
 
@@ -9,14 +8,15 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-@app.post("/answer/")
-async def get_answer(question_data: Dict[str, str]):
-    if 'question_text' not in question_data:
-        raise HTTPException(status_code=400, detail="Question text not provided")
-    response = "Ran succefully"
-    # Assuming milvus_retriver returns a dictionary, add 'response' parameter to it
+@app.route("/answer/", methods=['POST'])
+def get_answer():
+    question_data = request.get_json()
+    if not question_data or 'question_text' not in question_data:
+        return jsonify({"detail": "Question text not provided"}), 400
+
+    response = "Ran successfully"
     response_with_parameter = {'response': response}
-    return response_with_parameter
+    return jsonify(response_with_parameter)
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
